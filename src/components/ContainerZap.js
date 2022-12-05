eb05b9737b2af7f7669ecaa051ca35000acbc6d0
Nathalya-Pires <111201469+Nathalya-Pires@users.noreply.github.com>
2022-12-05 16:54:02 -0400

    Bonus 1 adicionado

--- src/components/ContainerZap.js
+++ src/components/ContainerZap.js
@@ -5,21 +5,40 @@ import Rodape from "./Rodape"
 import { useState } from "react"
 import ContainerTela from "../style/ContainerTela"
 
+
 export default function ContainerZap() {
   const [contador, setContador] = useState(0)
+  const [boasVindas, setBoasVindas] = useState(true)
 
+  if (boasVindas === true) {
 
     return (
-        <ContainerTela>
-            <Logo>
-                <img src={logo} alt="Logo de Raio"></img>
-                <h1>ZapRecall</h1>
-            </Logo>
-            <FlashCards contador={contador} setContador={setContador}/>
-            <Rodape contador={contador}/>
-        </ContainerTela>
+      <ContainerTela>
+        <LogoInicial>
+          <img src={logo} alt="Logo de Raio"></img>
+          <h1>ZapRecall</h1>
+        </LogoInicial>
+        <BotaoInicial onClick={() => setBoasVindas(false)}>Iniciar Recall!</BotaoInicial>
+      </ContainerTela>
+    )
+  }
+
+  if (boasVindas === false) {
+
+    return (
+      <ContainerTela>
+        <Logo>
+          <img src={logo} alt="Logo de Raio"></img>
+          <h1>ZapRecall</h1>
+        </Logo>
+        <FlashCards contador={contador} setContador={setContador} />
+        <Rodape contador={contador} />
+      </ContainerTela>
 
     )
+
+  }
+
 
 }
 
@@ -43,4 +62,40 @@ const Logo = styled.div`
   margin-left: 20px;
   }
 
+`
+
+const LogoInicial = styled.div`
+   margin-top: 8%;
+
+   h1{
+    font-family: 'Righteous';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+    color: #FFFFFF;
+    margin-top: 9%;
+    
+ }
+`
+
+const BotaoInicial = styled.button`
+    width: 30%;
+    height: 54px;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #D70900;
+    background-color: #FFFFFF;
+    border:1px solid #D70900;
+    border-radius: 5px ;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+    margin-top: 3%;
+    cursor: pointer;
 `
